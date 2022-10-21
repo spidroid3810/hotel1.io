315530eec6d16f71ab5e1d17fd0b3f0b5322b008
spidroid381 <102158589+spidroid3810@users.noreply.github.com>
2022-10-21 14:45:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -28,3 +28,14 @@
  });
  }
  
+ 
+ var myVar;
+ 
+ function myFunction() {
+ myVar = setTimeout(showPage, 3900);
+ }
+ 
+ function showPage() {
+ document.getElementById("loader").style.display = "none";
+ document.getElementById("myDiv").style.display = "block";
+ }
