94f4a94c2ac2ff642b5ad77121e5cf1e38f89b6e
spidroid381 <102158589+spidroid3810@users.noreply.github.com>
2022-10-21 13:28:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -19,4 +19,12 @@
  }
  
  
+ var closebtns = document.getElementsByClassName("close");
+ var i;
+ 
+ for (i = 0; i < closebtns.length; i++) {
+ closebtns[i].addEventListener("click", function() {
+ this.parentElement.style.display = 'none';
+ });
+ }
  
